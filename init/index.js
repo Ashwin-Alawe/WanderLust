1701a8aaa187b4abb40fe9442deb7274bbb76f32
ashwin <ashwinalawe@gmail.com>
2025-02-12 08:46:00 +0530

    Phase 1 part A

--- init/index.js
+++ init/index.js
@@ -7,7 +7,7 @@ main().then(() =>{
 }) .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlst');
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
  }
 
  const initDB = async () =>{
--- models/listing.js
+++ models/listing.js
@@ -9,10 +9,10 @@ const listingSchema = new Schema({
     description: String, 
     image: {
         type: String,
+          // type: String,
         default: "https://cdn.pixabay.com/photo/2017/09/23/04/02/dice-2777809_1280.jpg",
-        set: (v) => v === ""? "https://cdn.pixabay.com/photo/2017/09/23/04/02/dice-2777809_1280.jpg"
-        : v,
-    },
+        set: (v) => v === "" ? "https://cdn.pixabay.com/photo/2017/09/23/04/02/dice-2777809_1280.jpg" : v,
+      },   
     price: Number,
     location: String,
     country: String
