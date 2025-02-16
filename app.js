666963c70b92c9ff9db8a981f773d5b9c55a7d09
ashwin <ashwinalawe@gmail.com>
2025-02-16 14:01:00 +0530

    Phase 2 - Flash messages added for success and errors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 main().then(() =>{
     console.log("connected to DB");
@@ -24,8 +26,28 @@ async function main() {
  app.engine('ejs', ejsMate);  
  app.use(express.static(path.join(__dirname, "/public")));
 
+ const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+ }
+
 app.get("/", (req, res) =>{
     res.send("Hi, I am root");
+})
+
+ app.use(session(sessionOptions));
+ app.use(flash());
+
+app.use((req, res, next) =>{
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
 })
 
 app.use("/listings", listings);
--- routes/review.js
+++ routes/review.js
@@ -25,8 +25,11 @@ router.post("/", validateReview, wrapAsync(async (req, res) => {
     let newReview = new Review(req.body.review);
 
     listing.reviews.push(newReview);
+
     await newReview.save();
     await listing.save();
+
+    req.flash("success", "New Review Added");
 
     res.redirect(`/listings/${listing._id}`);
 }))
@@ -36,6 +39,8 @@ router.delete("/:reviewId", wrapAsync(async (req, res) =>{
     let {id, reviewId} = req.params;
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
+
+    req.flash("success", "Review Deleted");
 
     res.redirect(`/listings/${id}`);
 }))
